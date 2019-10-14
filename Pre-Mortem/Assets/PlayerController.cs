8db92fa07c2acadefbaae41e4315f7c7fbb39049
ReidyOG <49667866+ReidyOG@users.noreply.github.com>
2019-10-14 14:32:00 +1100

    Added movement + some assets

---
+++ Pre-Mortem/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        float x_move = Input.GetAxisRaw("Horizontal");
+        float z_move = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * x_move;
+        Vector3 moveVertical = transform.forward * z_move;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        motor.Move(velocity);
+    }
+}
---
+++ Pre-Mortem/Assets/PlayerMotor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    private Vector3 velocity = Vector3.zero;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move (Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+    }
+
+    void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.deltaTime);
+        }
+    }
+}
