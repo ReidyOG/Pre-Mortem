59e666b49f59e06c40d6f300963f1450e2a99c68
Yash Sabnis <yas904@uowmail.edu.au>
2019-10-15 21:11:06 +1100

    Shooting Scripts Finished
    - Added a shooting script that spawns a ball at a point (I had to make a cude inside the gun)
    - Also made it so that the bullets can be deleted after a customisable amount of seconds
    - Added a second camera that uses display 2, just to use for testing purposes

---
+++ Pre-Mortem/Assets/Shooting Assets/ShootBullet.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Shooting_Assets
+{
+    public class ShootBullet : MonoBehaviour
+    {
+        public GameObject projectile;
+        public float speed;
+        public float despawn;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                GameObject gun = GameObject.FindWithTag("Gun");
+                GameObject cam = GameObject.FindWithTag("MainCamera");
+                Vector3 front = gun.transform.position;
+                Debug.Log(front.ToString());
+                GameObject bullet = Instantiate(projectile, front, gun.transform.rotation);
+                Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
+                bulletRigidbody.velocity = transform.TransformDirection(0, 0, speed);
+                Destroy(bullet, despawn);
+            }
+        }
+    }
+}
